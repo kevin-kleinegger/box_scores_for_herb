6602f68701b3e35192ee2cf73f8a3bb14bbe6df2
Kevin Kleinegger <kevinkleinegger@kevins-air.myfiosgateway.com>
2023-12-09 23:13:56 -0500

    add in date endpoints

--- box_scores.py
+++ box_scores.py
@@ -4,23 +4,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-def generate_data():
-    date_of_interest = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-    games = statsapi.schedule(date=date_of_interest)
+def generate_data(d=(datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')):
+    games = statsapi.schedule(date=d)
     teams = {t['teamName']:t['id'] for t in statsapi.get('teams', {'sportIds':1})['teams']}
     if len(games)==0:
-        date_of_interest = get_last_day_of_baseball(teams)
-        games = statsapi.schedule(date=date_of_interest)
-    f = open("box_scores.txt", "w", encoding='utf-8')
+        d = get_last_day_of_baseball(teams)
+        games = statsapi.schedule(date=d)
     box_scores = []
     for g in games:
         box_scores.append(statsapi.boxscore(g['game_id']))
-    return box_scores
+    return box_scores, d
 
 @app.route('/')
 def index():
-    return render_template('index.html', box_scores=generate_data())
-    
+    box_scores, dd = generate_data()
+    return render_template('index.html', box_scores=box_scores, default_date=dd)
+
+@app.route('/<date>')
+def display_box_scores(date):
+    box_scores, date = generate_data(date)
+    return render_template('index.html', box_scores=box_scores, default_date=date)
 
 def get_last_day_of_baseball(teams):
     most_recent_day = 0
